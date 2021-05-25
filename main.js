8f66500dc18a71cce1434aa1338ff685b23fd638
AMichniov <84792726+AMichniov@users.noreply.github.com>
2021-05-25 19:29:31 +0300

    mano

---
+++ main.js
@@ -0,0 +1,18 @@
+// kintamuju galiojimo sritis
+
+// globoli aplinka/ global Scope
+var a= 4;
+console.log (a)
+
+function musu() {
+   // lokali aplinka/ local scope
+var b = 5;
+}
+
+function kita(){
+   var c =5 
+}
+// kas yra sukurta funkcijoi, to negalima kitur pasekt tik toje funkcijoje
+
+let e = 7 
+console.log(e)
